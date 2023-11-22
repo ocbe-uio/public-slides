676ba193fa5ad55252fc3976913e23c7eeae0403
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-11-22 06:50:41 +0100

    Ordered presentation list

--- recreate-presentations-index.py
+++ recreate-presentations-index.py
@@ -8,10 +8,12 @@ exclude = ["aux", "index.html"]
 # Generating HTML
 out = open("Presentations/index.html", mode = "w")
 out.write("<h1>Presentations</h1>\n")
+out.write("<ol>")
 for ff in files:
   if ff in exclude:
     continue
   host = "https://ocbe-uio.github.io/public-slides/Presentations/"
-  link = "<a href='" + host + ff + "'>" + ff + "</a><br>\n"
+  link = "<li><a href='" + host + ff + "'>" + ff + "</a></li>\n"
   out.write(link)
+out.write("</ol>")
 out.close()
